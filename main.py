6efe467756ff2dc12db982f93240c587f92d4769
michelhugo <hugo.michel@epfl.ch>
2021-04-15 17:46:13 +0200

    1000 episodes NN

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ MODE = 'train'
 
 def main():
     # Fetch data
-    dataset, ages, spawns, no_logs = extract_logs(path=PATH, n=1000, from_=700, to=850)
+    dataset, ages, spawns, no_logs = extract_logs(path=PATH, n=1000, from_=850, to=1000)
     sums_of_rewards = run(dataset, ages, spawns, no_logs, model_path, MODE)
     plot(sums_of_rewards)
 
